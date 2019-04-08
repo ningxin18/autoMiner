38f492f5a5fc1fec936aaf73d3b0b943dc5a7665
ningxin18 <292409083@qq.com>
2019-04-08 18:31:43 +0800

    check OS

--- linux/verifyAndMiner.sh
+++ linux/verifyAndMiner.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+OS="LINUX"
+LinuxConfigDir="${HOME}/.usechain"
+OSXConfigDir=${HOME}/Library/usechain
+WindowsConfigDir=${HOME}/AppData/Roaming/usechain
+ConfigDir="$LinuxConfigDir"
+
+case "$OSTYPE" in
+  solaris*) OS="SOLARIS" ;;
+  linux*)   OS="LINUX" ;;
+  bsd*)     OS="BSD" ;;
+  darwin*)  OS="OSX" ; ConfigDir=$OSXConfigDir;; # Mac
+  msys*)    OS="WINDOWS" ; ConfigDir=$WindowsConfigDir;;# Git Bash/msysGit
+  cygwin*)  OS="WINDOWS" ; ConfigDir=$WindowsConfigDir;; # Cygwin
+  *)        OS="UNKNOWN"; echo "unknown: $OSTYPE" ;;
+esac
+
+echo
+echo "OS:$OS"
+echo "Usechain ConfigDir: $ConfigDir"
+if [ ${OS}x == "UNKNOWN"x ]; then
+    echo "error OS $OSTYPE not supported, exit ..."
+    exit;
+fi
+
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+
+# make config directory for user
+if [ ! -d $ConfigDir ]; then
+    echo "mkdir $ConfigDir"
+    mkdir -p $ConfigDir
+fi
+
+
 autoMiner="./autoMiner"
 if [ ! -f "$autoMiner" ];then
 	go build ./autoMiner.go
@@ -10,10 +43,10 @@ if [ $count -eq 0 ]; then
         nohup ./used --rpc --rpcaddr="0.0.0.0" --rpccorsdomain "*"  --rpcapi="web3,eth,admin,personal,miner,net" --gasprice "10"  --txpool.pricelimit "10"  --verbosity 3  &
 fi
 
-userCert="$HOME/.usechain/user.crt"
+userCert="$ConfigDir/user.crt"
 if [ ! -f "$userCert" ]; then
- 
-userDataFile="$HOME/.usechain/userData.json"
+
+userDataFile="$ConfigDir/userData.json"
 
 #check userdata.json
 if [ ! -f "$userDataFile" ]; then
@@ -75,5 +108,3 @@ echo "You already have user.crt, you can register and miner directly, please con
 #autominer include register miner, start miner
 sleep 3
 ./autoMiner
-
-
